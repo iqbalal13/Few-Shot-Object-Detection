c3ffe695ed1061b0c05513f1a1b2a72e6d344765
Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>
2026-07-09 17:41:40 +0700

    Create STEP 5 : Initialize Prototype Extractor.py
    Signed-off-by: Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>

---
+++ META DETR/SCRIPT/STEP 5/STEP 5 : Initialize Prototype Extractor.py
@@ -0,0 +1,7 @@
+# ==========================================================
+# STEP 5 : Initialize Prototype Extractor
+# ==========================================================
+
+prototype_extractor = PrototypeFeatureExtractor().to(CONFIG["device"])
+
+print(prototype_extractor)
