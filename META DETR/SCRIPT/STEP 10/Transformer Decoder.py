fd9bbb71a188547a2b0de000018859269317cb7e
Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>
2026-07-11 16:53:09 +0700

    Create Transformer Decoder.py
    Signed-off-by: Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>

---
+++ META DETR/SCRIPT/STEP 10/Transformer Decoder.py
@@ -0,0 +1,50 @@
+ ==========================================================
+# STEP 10 : Transformer Decoder
+# ==========================================================
+
+import torch
+import torch.nn as nn
+
+class TransformerDecoder(nn.Module):
+
+    def __init__(
+        self,
+        hidden_dim=2048,
+        num_heads=8,
+        num_layers=6,
+        num_queries=100
+    ):
+
+        super().__init__()
+
+        self.object_queries = nn.Parameter(
+            torch.randn(num_queries, hidden_dim)
+        )
+
+        decoder_layer = nn.TransformerDecoderLayer(
+            d_model=hidden_dim,
+            nhead=num_heads,
+            batch_first=True
+        )
+
+        self.decoder = nn.TransformerDecoder(
+            decoder_layer,
+            num_layers=num_layers
+        )
+
+    def forward(self, encoder_output):
+
+        batch_size = encoder_output.size(0)
+
+        queries = self.object_queries.unsqueeze(0).repeat(
+            batch_size,
+            1,
+            1
+        )
+
+        output = self.decoder(
+            tgt=queries,
+            memory=encoder_output
+        )
+
+        return output
