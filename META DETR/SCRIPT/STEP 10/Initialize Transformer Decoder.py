f7a356992fe4c6dd13b7edde4d8d71beff4c4470
Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>
2026-07-11 16:53:32 +0700

    Create Initialize Transformer Decoder.py
    Signed-off-by: Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>

---
+++ META DETR/SCRIPT/STEP 10/Initialize Transformer Decoder.py
@@ -0,0 +1,12 @@
+# ==========================================================
+# STEP 10 : Initialize Transformer Decoder
+# ==========================================================
+
+transformer_decoder = TransformerDecoder(
+    hidden_dim=2048,
+    num_heads=CONFIG["num_heads"],
+    num_layers=CONFIG["num_decoder_layers"],
+    num_queries=CONFIG["num_queries"]
+).to(CONFIG["device"])
+
+print(transformer_decoder)
