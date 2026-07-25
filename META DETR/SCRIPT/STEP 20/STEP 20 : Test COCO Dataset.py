164a153962fd95e0ab13dbc5a03bbf6e25819659
Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>
2026-07-25 22:10:55 +0700

    Update STEP 20 : Test COCO Dataset.py
    Signed-off-by: Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>

--- META DETR/SCRIPT/STEP 20/STEP 20 : Test COCO Dataset.py
+++ META DETR/SCRIPT/STEP 20/STEP 20 : Test COCO Dataset.py
@@ -1,11 +1,21 @@
 # ==========================================================
-# STEP 20 : Test COCO Dataset
+# STEP 20 : Test COCO Episodic Dataset
 # ==========================================================
 
-image, target = train_dataset[0]
+episode = train_dataset[0]
+
+support_image = episode["support_image"]
+support_target = episode["support_target"]
+
+query_image = episode["query_image"]
+query_target = episode["query_target"]
 
 print("=" * 60)
-print("Image Shape :", image.shape)
-print("Number of Boxes :", len(target["boxes"]))
-print("Number of Labels :", len(target["labels"]))
+print("Support Image Shape :", support_image.shape)
+print("Support Boxes       :", len(support_target["boxes"]))
+print("Support Labels      :", len(support_target["labels"]))
+print("-" * 60)
+print("Query Image Shape   :", query_image.shape)
+print("Query Boxes         :", len(query_target["boxes"]))
+print("Query Labels        :", len(query_target["labels"]))
 print("=" * 60)
