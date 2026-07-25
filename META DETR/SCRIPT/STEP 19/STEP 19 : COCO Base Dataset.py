64b15a25328949a405f8a40cd3093e3d24a43b39
Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>
2026-07-25 22:15:33 +0700

    Update STEP 19 : COCO Base Dataset.py
    Signed-off-by: Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>

--- META DETR/SCRIPT/STEP 19/STEP 19 : COCO Base Dataset.py
+++ META DETR/SCRIPT/STEP 19/STEP 19 : COCO Base Dataset.py
@@ -1,13 +1,14 @@
 # ==========================================================
-# STEP 19 : COCO Base Dataset
+# STEP 19 : COCO Episode Dataset
 # ==========================================================
 
 from torch.utils.data import Dataset
 from PIL import Image
 import torch
 import os
+import random
 
-class COCOBaseDataset(Dataset):
+class COCOEpisodeDataset(Dataset):
 
     def __init__(self, coco, image_dir, transform=None):
 
@@ -23,12 +24,36 @@ class COCOBaseDataset(Dataset):
             for idx, cat_id in enumerate(cat_ids)
         }
 
+        # --------------------------------------------------
+        # Build Class -> Image Index
+        # --------------------------------------------------
+        self.class_to_images = {}
+
+        for image_id in self.image_ids:
+
+            ann_ids = self.coco.getAnnIds(imgIds=image_id)
+            anns = self.coco.loadAnns(ann_ids)
+
+            classes = set()
+
+            for ann in anns:
+                cls = self.cat2label[ann["category_id"]]
+                classes.add(cls)
+
+            for cls in classes:
+
+                if cls not in self.class_to_images:
+                    self.class_to_images[cls] = []
+
+                self.class_to_images[cls].append(image_id)
+
     def __len__(self):
         return len(self.image_ids)
 
-    def __getitem__(self, index):
-
-        image_id = self.image_ids[index]
+    # ------------------------------------------------------
+    # Load Single Image + Target
+    # ------------------------------------------------------
+    def load_single_sample(self, image_id):
 
         image_info = self.coco.loadImgs(image_id)[0]
 
@@ -54,7 +79,6 @@ class COCOBaseDataset(Dataset):
 
             boxes.append([cx, cy, w, h])
 
-            # gunakan class index 0-79
             labels.append(
                 self.cat2label[ann["category_id"]]
             )
@@ -87,6 +111,67 @@ class COCOBaseDataset(Dataset):
                 image_id,
                 dtype=torch.long
             )
+
+        }
+
+        return image, target
+
+    # ------------------------------------------------------
+    # Episodic Sampling
+    # ------------------------------------------------------
+    def __getitem__(self, index):
+
+        support_id = self.image_ids[index]
+
+        ann_ids = self.coco.getAnnIds(imgIds=support_id)
+        anns = self.coco.loadAnns(ann_ids)
+
+        # Jika gambar tidak punya anotasi
+        if len(anns) == 0:
+
+            support_image, support_target = \
+                self.load_single_sample(support_id)
+
+            return {
+                "support_image": support_image,
+                "support_target": support_target,
+                "query_image": support_image,
+                "query_target": support_target
+            }
+
+        # Pilih salah satu class pada support image
+        support_class = random.choice(anns)["category_id"]
+        support_class = self.cat2label[support_class]
+
+        candidate_images = self.class_to_images[support_class]
+
+        # Cari query image yang berbeda
+        if len(candidate_images) > 1:
+
+            query_id = support_id
+
+            while query_id == support_id:
+                query_id = random.choice(candidate_images)
+
+        else:
+
+            query_id = support_id
+
+        support_image, support_target = \
+            self.load_single_sample(support_id)
+
+        query_image, query_target = \
+            self.load_single_sample(query_id)
+
+        return {
+
+            "support_image": support_image,
+            "support_target": support_target,
+
+            "query_image": query_image,
+            "query_target": query_target
+
+        }
 
         }
 
