f1ee425d8473574f8eb05e0a50e89296f4ba3696
Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>
2026-07-27 21:29:10 +0700

    Create STEP 1 : Import Libraries.py
    Signed-off-by: Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>

---
+++ META DETR/SCRIPT/STEP 1/STEP 1 : Import Libraries.py
@@ -0,0 +1,17 @@
+# ==========================================================
+# STEP 1 : Import Libraries
+# ==========================================================
+
+import os
+import cv2
+import torch
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+from PIL import Image
+from tqdm import tqdm
+
+print("=" * 60)
+print("Libraries imported successfully")
+print("=" * 60)
