fad9c28d698b73b3c3925940ad8acc7083042a18
Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>
2026-08-25 19:04:55 +0700

    Delete META DETR/SCRIPT/STEP 4/STEP 4 REMOVE CLASSIFICATION LAYER.py
    Signed-off-by: Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>

--- META DETR/SCRIPT/STEP 4/STEP 4 REMOVE CLASSIFICATION LAYER.py
+++
@@ -1,9 +0,0 @@
-# ==========================================================
-# STEP 4 : Remove Classification Layer
-# ==========================================================
-
-backbone = nn.Sequential(
-    *list(resnet101.children())[:-2]
-)
-
-print(backbone)
