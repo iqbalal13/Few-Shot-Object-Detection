e462072625163cba6f46eb4ebc7901ca1dc57b50
Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>
2026-08-25 19:06:31 +0700

    Delete META DETR/SCRIPT/STEP 5/STEP 5 : Initialize Prototype Extractor.py
    Signed-off-by: Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>

--- META DETR/SCRIPT/STEP 5/STEP 5 : Initialize Prototype Extractor.py
+++
@@ -1,7 +0,0 @@
-# ==========================================================
-# STEP 5 : Initialize Prototype Extractor
-# ==========================================================
-
-prototype_extractor = PrototypeFeatureExtractor().to(CONFIG["device"])
-
-print(prototype_extractor)
