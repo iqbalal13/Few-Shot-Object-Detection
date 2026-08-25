20c61461de90aeceab3ee3d7a52a22f647282a5c
Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>
2026-08-25 19:12:22 +0700

    Delete META DETR/SCRIPT/STEP 7 directory
    Signed-off-by: Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>

--- META DETR/SCRIPT/STEP 7/Initialize Query Encoder.py
+++
@@ -1,7 +0,0 @@
-# ==========================================================
-# STEP 7 : Initialize Query Encoder
-# ==========================================================
-
-query_encoder = QueryEncoder(backbone).to(CONFIG["device"])
-
-print(query_encoder)
